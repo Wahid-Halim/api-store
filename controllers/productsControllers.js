e39b03cd92dd1e8b92ed285ed78f05a0caeea9a6
Wahid Halim <wahidkeo@gmail.com>
2025-11-09 15:44:42 +0430

    test getAllProductsStatic with find({})

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,5 +1,11 @@
+const Product = require("../models/productModel");
+
 const getAllProductStatic = async (req, res) => {
-  res.status(200).json({ msg: "Products testing route" });
+  const products = await Product.find({
+    company: "ikea",
+    featured: true,
+  });
+  res.status(200).json({ status: "success", data: [products] });
 };
 
 const getAllProducts = async (req, res) => {
