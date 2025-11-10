50ab7bca216e79f2326089133cffefc916e7e268
Wahid Halim <wahidkeo@gmail.com>
2025-11-10 06:53:52 +0430

    Add query operators to name property

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -2,16 +2,19 @@ const Product = require("../models/productModel");
 
 const getAllProductStatic = async (req, res) => {
   const products = await Product.find({
-    page: 3,
+    name: "wahid",
   });
-  res.status(200).json({ status: "success", data: [products] });
+  res.status(200).json({ products, nHabits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
   if (featured) queryObject.featured = featured === "true" ? true : false;
   if (company) queryObject.company = company;
+  if (name) queryObject.name = { $regex: name, $options: "i" };
+  console.log(name);
+  console.log(queryObject);
 
   const products = await Product.find(queryObject);
   res.status(200).json({ products, nHabits: products.length });
