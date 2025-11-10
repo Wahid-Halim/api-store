2892b6df192bca4341813dcf70ce32cc62fd7206
Wahid Halim <wahidkeo@gmail.com>
2025-11-10 06:33:19 +0430

    Add compnay query search feature to api

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -2,14 +2,18 @@ const Product = require("../models/productModel");
 
 const getAllProductStatic = async (req, res) => {
   const products = await Product.find({
-    company: "ikea",
-    featured: true,
+    page: 3,
   });
   res.status(200).json({ status: "success", data: [products] });
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured, company } = req.query;
+  const queryObject = {};
+  if (featured) queryObject.featured = featured === "true" ? true : false;
+  if (company) queryObject.company = company;
+
+  const products = await Product.find(queryObject);
   res.status(200).json({ products, nHabits: products.length });
 };
 
