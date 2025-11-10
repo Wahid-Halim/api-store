e8ec2e55c202e02074e2ac276c85a21588c8481b
Wahid Halim <wahidkeo@gmail.com>
2025-11-10 08:46:43 +0430

    add option select to api

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,26 +1,32 @@
 const Product = require("../models/productModel");
 
 const getAllProductStatic = async (req, res) => {
-  const products = await Product.find({
-    name: "wahid",
-  });
+  const products = await Product.find({}).select("name price");
   res.status(200).json({ products, nHabits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) queryObject.featured = featured === "true" ? true : false;
   if (company) queryObject.company = company;
   if (name) queryObject.name = { $regex: name, $options: "i" };
 
   let result = Product.find(queryObject);
+  //sort
   if (sort) {
-    const shortList = sort.split(",").join(" ");
+    const sortList = sort.split(",").join(" ");
     result = result.sort(shortList);
   } else {
     result = result.sort("createdAt");
   }
+
+  //fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
   const products = await result;
 
   res.status(200).json({ products, nHabits: products.length });
