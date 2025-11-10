b8fb9faa79abe4f067372e02987e3c28c12a2c59
Wahid Halim <wahidkeo@gmail.com>
2025-11-10 07:35:31 +0430

    add sort to allproducts

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -8,15 +8,21 @@ const getAllProductStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
   if (featured) queryObject.featured = featured === "true" ? true : false;
   if (company) queryObject.company = company;
   if (name) queryObject.name = { $regex: name, $options: "i" };
-  console.log(name);
-  console.log(queryObject);
 
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const shortList = sort.split(",").join(" ");
+    result = result.sort(shortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
+
   res.status(200).json({ products, nHabits: products.length });
 };
 
